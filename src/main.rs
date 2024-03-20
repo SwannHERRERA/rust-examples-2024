ba5d3def39d46da5f0674a5780340a2b64ef1e79
Swann HERRERA <swann.herrera@nuant.com>
2024-03-20 10:04:30 +0100

    fix logger

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::collections::HashMap;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 use std::time::Duration;
-use tracing::{info, Level};
+use tracing::{info, trace, Level};
+use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::FmtSubscriber;
 
@@ -21,9 +22,9 @@ fn clean_terminal() {
     print!("\x1B[2J\x1B[1;1H");
 }
 
-fn configure_logger() {
+fn configure_logger() -> WorkerGuard {
     let file_appender = tracing_appender::rolling::daily("./logs", "prefix.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
 
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
@@ -33,6 +34,7 @@ fn configure_logger() {
 
     tracing::subscriber::set_global_default(subscriber)
         .expect("Impossible de configurer le subscriber global de tracing");
+    guard
 }
 
 fn update_position(id: u32, command_rx: Receiver<Command>, tx: Sender<Message>) {
@@ -42,6 +44,7 @@ fn update_position(id: u32, command_rx: Receiver<Command>, tx: Sender<Message>) 
             Command::Move => {
                 let dx = rng.gen_range(-1..=1);
                 let dy = rng.gen_range(-1..=1);
+                trace!("dx {}, dy: {}", dx, dy);
                 tx.send(Message::NewPosition { id, dx, dy })
                     .expect("Failed to send position");
             }
@@ -65,7 +68,7 @@ const MAX_WEIGTH: usize = 20;
 const TICK_DURATION: Duration = Duration::from_millis(10);
 
 fn main() {
-    configure_logger();
+    let _guard = configure_logger();
     info!("DEBuG");
     let (tx, rx) = mpsc::channel::<Message>();
     let mut command_txs = vec![];
@@ -95,14 +98,13 @@ fn main() {
         for _ in 0..NB_ROBOTS {
             if let Ok(Message::NewPosition { id, dx, dy }) = rx.recv() {
                 if let Some(position) = positions.get_mut(&id) {
-                    position.0 = (position.0 + dx).max(0).min(MAX_WEIGTH as i32 - 1);
-                    position.1 = (position.1 + dy).max(0).min(MAX_HEIGHT as i32 - 1);
+                    position.0 = (position.0 + dx).clamp(0, MAX_WEIGTH as i32 - 1);
+                    position.1 = (position.1 + dy).clamp(0, MAX_HEIGHT as i32 - 1);
                 }
                 map.iter_mut()
                     .for_each(|row| row.iter_mut().for_each(|c| *c = ' '));
 
                 for (&id, &(x, y)) in &positions {
-                    info!("x {} y {}", x, y);
                     map[y as usize][x as usize] = match id {
                         0 => '@',
                         1 => '%',
